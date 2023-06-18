b5dd47503cdc88cf599a9a4866162458952b06ea
Prakashsoni12 <85999331+Prakashsoni12@users.noreply.github.com>
2023-06-18 19:59:30 +0530

    Update settings.py
    sec_rem

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!f#w96dr$fxq$oz+_to+zz)-*l4x9lvh$yv08%-fbms&naqmyv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
